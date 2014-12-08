5a7113abc38b701b9720e7d49ef5232da118dbe4
Carlos Souza <carloshrsouza@gmail.com>
2014-12-08 11:11:24 -0500

    Checks for 140 char limit.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.post('/tweet', bodyParser.urlencoded({ extended: false }),  function(request, response) {
+  var description = request.body.description;
+
+  if(!description || description.length > 140){
+    response.status(400).json("This Tweet cannot be Tweeted.");
+  }else{
+    response.status(200).json("This Tweet can be Tweeted.")
+  }
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
